8c9403b37ce9d4e8f3e5f968b8f1d543bfcf5128
Nahda Ayuni Narindri <100254212+nahdaayuni@users.noreply.github.com>
2022-03-19 10:02:01 +0700

    Add files via upload

---
+++ src/Component/beranda.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class beranda extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>ini Beranda</h2>
+            </div>
+        )
+    }
+}
+
+export default beranda;
